24b41f050bf35325dfa32112c69a7f7e2adf5961
Stephen Muraya <gathaiya28@gmail.com>
2021-06-17 17:10:26 +0300

    UI DESIGN 1.01
    more html pages and javascript tools..... and changes on initial ui pages

--- Designs/js/motor-benefits.js
+++ Designs/js/motor-benefits.js
@@ -3,6 +3,7 @@
 /////////////////////////////////////////////////////////
 var cover = document.getElementById("cover_type");
 var sum_insured = document.getElementById("sum_insured_input");
+var sum_insured_poa = document.getElementById("sum_insured_poa");
 var rate = document.getElementById("rate_input");
 var policy_remarks = document.getElementById("policy_remarks");
 
@@ -12,16 +13,47 @@ cover.addEventListener("change", function () {
     cover.options[cover.selectedIndex].value == "Comprehensive"
   ) {
     rate.style.display = "initial";
+    rate.required = true;
     sum_insured.placeholder = "Sum Insured";
     policy_remarks.rows = "8";
     policy_remarks.style.maxHeight = "141px";
   } else {
     rate.style.display = "none";
+    rate.required = false;
     sum_insured.placeholder = "Basic Premium";
     policy_remarks.rows = "12";
     policy_remarks.style.maxHeight = "180px";
   }
 });
+
+//////////////////////////////////////////////////////////
+// CASH CONCUTINATION JS
+/////////////////////////////////////////////////////////
+function cashConcut() {
+  var cash = sum_insured.value;
+  var clean_up = cash.replace("Kshs ", "");
+  var clean_up2 = clean_up
+    .replace(/,/g, "")
+    .replace("N", "")
+    .replace("NaN", "")
+    .replace("n", "")
+    .replace("E", "")
+    .replace("e", "")
+    .replace("A", "")
+    .replace("a", "")
+    .replace("Q", "")
+    .replace("q", "");
+
+  if (clean_up2 == "") {
+    sum_insured.value = "";
+  } else {
+    var no_Nans = clean_up2.replace("NaN", "");
+    var no_zeros = parseInt(no_Nans);
+    var new_cash = no_zeros.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    sum_insured.value = "Kshs " + new_cash;
+    sum_insured_poa.value = no_zeros;
+  }
+}
 
 //////////////////////////////////////////////////////////
 // MV CLASS SELECTION CODE STATEMENT
@@ -59,41 +91,77 @@ var windscreen_benefit = document.getElementById("windscreen_benefit");
 var windscreen_benefit_basic_premium = document.getElementById(
   "windscreen_benefit_basic_premium"
 );
+var personal_accident = document.getElementById("personal_accident");
+var personal_accident_basic_premium = document.getElementById(
+  "personal_accident_basic_premium"
+);
+var loss_of_use = document.getElementById("loss_of_use");
+var loss_of_use_basic_premium = document.getElementById(
+  "loss_of_use_basic_premium"
+);
 
 ep.addEventListener("change", function () {
   if (ep.checked) {
     ep_basic_premium.disabled = false;
+    ep_basic_premium.required = true;
   } else {
     ep_basic_premium.disabled = true;
+    ep_basic_premium.required = true;
   }
 });
 
 windscreen_benefit.addEventListener("change", function () {
   if (windscreen_benefit.checked) {
     windscreen_benefit_basic_premium.disabled = false;
+    windscreen_benefit_basic_premium.required = true;
   } else {
     windscreen_benefit_basic_premium.disabled = true;
+    windscreen_benefit_basic_premium.required = false;
   }
 });
 
 pvt.addEventListener("change", function () {
   if (pvt.checked) {
     pvt_basic_premium.disabled = false;
+    pvt_basic_premium.required = true;
   } else {
     pvt_basic_premium.disabled = true;
+    pvt_basic_premium.required = false;
   }
 });
 rescue_benefit.addEventListener("change", function () {
   if (rescue_benefit.checked) {
     rescue_benefit_basic_premium.disabled = false;
+    rescue_benefit_basic_premium.required = true;
   } else {
     rescue_benefit_basic_premium.disabled = true;
+    rescue_benefit_basic_premium.required = false;
   }
 });
 pll.addEventListener("change", function () {
   if (pll.checked) {
     pll_benefit_basic_premium.disabled = false;
+    pll_benefit_basic_premium.required = true;
   } else {
     pll_benefit_basic_premium.disabled = true;
+    pll_benefit_basic_premium.required = false;
+  }
+});
+personal_accident.addEventListener("change", function () {
+  if (personal_accident.checked) {
+    personal_accident_basic_premium.disabled = false;
+    personal_accident_basic_premium.required = true;
+  } else {
+    personal_accident_basic_premium.disabled = true;
+    personal_accident_basic_premium.required = false;
+  }
+});
+loss_of_use.addEventListener("change", function () {
+  if (loss_of_use.checked) {
+    loss_of_use_basic_premium.disabled = false;
+    loss_of_use_basic_premium.required = true;
+  } else {
+    loss_of_use_basic_premium.disabled = true;
+    loss_of_use_basic_premium.required = false;
   }
 });
